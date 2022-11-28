7b5a72ee507922c2e16d19177fb1ceac7e8400b3
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-28 10:35:52 -0700

    display

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,19 +9,11 @@
 </head>
 <body>
     <div class="container py-12">
-        {{-- <div class="grid grid-cols-4  grid-rows-5  gap-y-4 gap-x-2 text-white">
-            <div class="bg-yellow-200">1</div>
-            <div class="bg-red-900">2</div>
-            <div class="bg-blue-900 col-span-2 row-span-2">3</div>
-            <div class="bg-purple-900">4</div>
-            <div class="bg-blue-900">5</div>
-        </div> --}}
-        <div class="grid grid-cols-3 gap-6">
-            <div class="col-span-2">
-                <iframe class=" w-full aspect-video" src="https://www.youtube.com/embed/r1ySsbIA7EU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <div class="col-span-1 bg-blue-600"></div>
-        </div>
+        <p class="bg-orange-700 text-white hidden sm:inline-block md:block ">Etiqueta en bloque</p>
+        <div class="bg-red-700 text-white">Etiqueta en bloque</div>
+
+        <span class="bg-green-700 text-white block">Etiqueta inline</span>
+        <a href="#" class="bg-yellow-300 text-white">Etiqueta inline</a>
     </div>
 </body>
 </html>
